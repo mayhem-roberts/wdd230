5c29854321b7ee29810406e08ce55d91d383a5fd
mayhem-roberts <reaganmoberts@gmail.com>
2021-02-15 17:12:14 -0700

    commit

---
+++ lesson6/js/windChill.js
@@ -0,0 +1,26 @@
+//get value of temp and speed
+let tempF = 34
+let speed = 12
+//call windChill function and store in windchill var
+let windchill = windChill(tempF, speed);
+//output windchill
+document.getElementById("tempF").innerHTML = " " + tempF + "&deg F";
+document.getElementById("speed").innerHTML = " " + speed + "mph";
+document.getElementById("windChill").innerHTML = windchill; 
+      
+function windChill(tempF, speed) {
+  //The formula for computing the wind chill factor is    f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16
+  let t = tempF;
+  let s = Math.pow(speed, .16);
+  let message = "No Wind Chill";
+    
+  if (tempF < 50 && speed > 3) {
+    f = 35.74 + 0.6215 * t - 35.75 * s + 0.4275 * t * s;
+    f = Math.round(f);
+    return " " + f + "&deg F";
+  }
+  else {
+    message;
+    return message;
+  }
+}
