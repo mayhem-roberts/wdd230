2097e12068347761a4ae8bee9e3b48a294ec9c9e
mayhem-roberts <reaganmoberts@gmail.com>
2021-02-20 09:10:11 -0700

    commit

---
+++ lesson7/js/lazyload.js
@@ -0,0 +1,27 @@
+const imagesToLoad = document.querySelectorAll("img[data-src]");
+
+function preloadImage(img) {
+    const src = img.getAttribute("data-src");
+    if(!src) {
+        return;
+    }
+
+    img.src = src;
+}
+
+const imgOptions = {};
+
+const imgObserver = new IntersectionObserver((entries, imgObserver) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return;
+        } else {
+            preloadImage(entry.target);
+            imgObserver.unobserve(entry.target);
+        }
+    });
+}, imgOptions);
+
+imagesToLoad.forEach(image => {
+    imgObserver.observe(image);
+});
