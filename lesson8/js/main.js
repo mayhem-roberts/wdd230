65e1d2b20bc3556b28697d060c01689e74019550
mayhem-roberts <reaganmoberts@gmail.com>
2021-02-27 22:48:47 -0700

    commit

--- lesson8/js/main.js
+++ lesson8/js/main.js
@@ -3,14 +3,6 @@ function toggleMenu() {
     document.getElementById("navigation").classList.toggle("hide");
 }
 
-// pancakes
-let date = new Date();
-let day = date.getDay();
-
-if (day != 5) {
-    document.getElementById("pancakes").style.display = "none";
-}
-
 // current date
 let options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
 document.getElementById("currentDate").textContent = new Date().toLocaleDateString('en-us', options);
