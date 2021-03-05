674751d2e5673769196fa485c5123d3e13dcc5ac
mayhem-roberts <reaganmoberts@gmail.com>
2021-03-04 23:00:28 -0700

    commit

--- lesson7/js/lazyload.js
+++ lesson7/js/lazyload.js
@@ -1,8 +1,8 @@
-let imagesToLoad = document.querySelectorAll('img[data-src]');
+const imagesToLoad = document.querySelectorAll('img[data-src]');
 
 const imgOptions = {
   threshold: 0,
-  rootMargin: "0px, 0px, 50px, 0px"
+  rootMargin: "0px 0px 50px 0px"
 };
 
 const loadImages = (image) => {
@@ -25,10 +25,12 @@ if('IntersectionObserver' in window) {
       }
     });
   }, imgOptions);
+
   imagesToLoad.forEach((img) => {
     observer.observe(img);
   });
-} else {
+} 
+else {
   imagesToLoad.forEach((img) => {
     loadImages(img);
   });
--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -1,23 +1,28 @@
-const requestURL = 'byui-cit230.github.io/canvas-referenced/latter-day-prophets.json';
-const prophets = jsonObject['prophets'];
+const requestURL = 'https://byui-cit230.github.io/lessons/lesson-09/data/latter-day-prophets.json';
 
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);  // temporary checking for valid response and data parsing
-    
-    for (let i = 0; i < prophets.length; i++ ) {
+    //console.table(jsonObject);  // temporary checking for valid response and data parsing
+    const prophets = jsonObject['prophets'];
+
+    const cards = document.querySelector('.cards'); //html output div class cards
+
+    prophets.forEach(prophet => {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         
-        h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
-        
-        card.appendChild(h2);
-        
-        document.querySelector('div.cards').appendChild(card); 
-    }
+        let img = document.createElement('img');
+
+        h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
+        img.setAttribute('src', prophet.imageurl)
+
+        card.append(h2)
+        card.append(img)
+        cards.append(card)
+    });   
   });
 
 
