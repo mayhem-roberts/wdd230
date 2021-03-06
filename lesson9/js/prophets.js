8329964e51c249b5abb55449bf8bd143e4d95629
mayhem-roberts <reaganmoberts@gmail.com>
2021-03-05 22:14:03 -0700

    commit

--- lesson9/js/prophets.js
+++ lesson9/js/prophets.js
@@ -13,14 +13,14 @@ fetch(requestURL)
     prophets.forEach(prophet => {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
-        
+        let p = document.createElement('p');
         let img = document.createElement('img');
 
         h2.innerHTML = `${prophet.name} ${prophet.lastname}`;
-        img.setAttribute('src', prophet.imageurl)
+        p.innerHTML = `Date of Birth: ${prophet.birthdate} <br> Place of birth: ${prophet.birthplace}`;
+        img.setAttribute('src', prophet.imageurl);
 
-        card.append(h2)
-        card.append(img)
+        card.append(h2, p, img)
         cards.append(card)
     });   
   });
