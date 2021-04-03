74bae861f797d52f7fe89ec965d28f6a68bb91af
mayhem-roberts <reaganmoberts@gmail.com>
2021-04-02 19:09:43 -0600

    commit

---
+++ final/js/forecast.js
@@ -0,0 +1,22 @@
+//forecast
+const apiforecast = 'https://api.openweathermap.org/data/2.5/forecast?id=5588842&APPID=22b3edccab117440bdc47835f3f06a5e&units=imperial';
+
+fetch(apiforecast)
+  .then((response) => response.json())
+  .then((forecastObject) => {
+      console.log(forecastObject)
+
+      const forecast = forecastObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+      const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+      for (let i = 0; i < forecast.length; i++) {
+        let date = new Date(forecast[i].dt_txt);
+        let image = 'https://openweathermap.org/img/w/' + forecast[i].weather[0].icon + '.png';
+        let description = forecast[i].weather.description;
+
+        document.getElementById(`day${i+1}`).innerHTML = days[date.getDay()];
+        document.getElementById(`img${i+1}`).setAttribute('src', image);
+        document.getElementById(`img${i+1}`).setAttribute('alt', description + 'image');
+        document.getElementById(`temp${i+1}`).innerHTML = (`${(forecast[i].main.temp).toFixed(0)}&deg F`); 
+      };
+  });
