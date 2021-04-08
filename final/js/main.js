0aa64baa11e51650c1d09aae92f184fa6c550ddf
mayhem-roberts <reaganmoberts@gmail.com>
2021-04-07 22:29:25 -0600

    commit

--- final/js/main.js
+++ final/js/main.js
@@ -3,6 +3,6 @@ function toggleMenu() {
     document.getElementById("navigation").classList.toggle("hide");
 }
 
-// current date
-let options = {weekday: "long", day: "numeric", month: "long", year: "numeric"};
-document.getElementById("currentDate").textContent = new Date().toLocaleDateString('en-us', options);
+// last modified date
+let date = document.lastModified;
+   document.getElementById("dateUpdated").textContent = "Last Updated: " + date;
